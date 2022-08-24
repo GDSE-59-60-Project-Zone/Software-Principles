bf360b6991b50292ea147802a193daa2e76194da
sanuv9683 <sanuv9683@gmail.com>
2022-08-24 12:31:52 +0530

    Open close principle demonstrated

--- 01_Open-Close-Principle/src/Main.java
+++ 01_Open-Close-Principle/src/Main.java
@@ -1,3 +1,8 @@
+import service.Account;
+import service.types.CurrentAccount;
+import service.types.SalarySaverAccount;
+import service.types.SavingAccount;
+
 /**
  * @author : Sanu Vithanage
  * @since : 0.1.0
@@ -11,12 +16,34 @@ public class Main {
      * */
     public static void main(String[] args) {
 
-        BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
+        /*BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
 
         bankAppWithdrawalService.depositToCurrentAccount(100000);
         bankAppWithdrawalService.withdrawFromCurrentAccount(5000);
 
         bankAppWithdrawalService.depositToSalarySaverAccount(200000);
-        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);
+        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);*/
+
+
+
+        /* Let's deposit for Savings Account Bank App Withdrawal Service */
+        Account savingAccount= new SavingAccount();
+        BankAppWithdrawalService bankAppWithdrawalService1 = new BankAppWithdrawalService(savingAccount);
+        bankAppWithdrawalService1.depositToAccount(100000);
+        bankAppWithdrawalService1.depositToAccount(40000);
+
+
+        Account currentAccount= new CurrentAccount();
+        BankAppWithdrawalService bankAppWithdrawalService2 = new BankAppWithdrawalService(currentAccount);
+        bankAppWithdrawalService2.depositToAccount(200000);
+        bankAppWithdrawalService2.depositToAccount(30000);
+
+
+        Account salaryServer= new SalarySaverAccount();
+        BankAppWithdrawalService bankAppWithdrawalService3 = new BankAppWithdrawalService(salaryServer);
+        bankAppWithdrawalService3.depositToAccount(500000);
+        bankAppWithdrawalService3.depositToAccount(100000);
+
+        /* This is what we called as Open / Close Principle */
     }
 }
