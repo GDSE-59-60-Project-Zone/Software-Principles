778d3d8da9291283cfbd209952589c3a9d6120a1
sanuv9683 <sanuv9683@gmail.com>
2022-08-24 12:05:15 +0530

    Violated the principle

--- 01_Open-Close-Principle/src/Main.java
+++ 01_Open-Close-Principle/src/Main.java
@@ -6,14 +6,17 @@ public class Main {
     /*SOLID*/
     /* O - Open / Close Principle */
     /*
-    *This principle states that the software entities should be open for extensions but close
-    * for modifications
-    * */
+     *This principle states that the software entities should be open for extensions but close
+     * for modifications
+     * */
     public static void main(String[] args) {
 
         BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
 
         bankAppWithdrawalService.depositToCurrentAccount(100000);
         bankAppWithdrawalService.withdrawFromCurrentAccount(5000);
+
+        bankAppWithdrawalService.depositToSalarySaverAccount(200000);
+        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);
     }
 }
