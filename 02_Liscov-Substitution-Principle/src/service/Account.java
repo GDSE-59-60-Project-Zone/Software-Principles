002b2d242e4f18beaf8b36923c47158f3a563046
sanuv9683 <sanuv9683@gmail.com>
2022-08-24 12:56:54 +0530

    Introduce Liskov-Substitution Principle

---
+++ 02_Liscov-Substitution-Principle/src/service/Account.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public abstract class Account {
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+}
